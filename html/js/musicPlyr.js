7a83de7d5e67038d095bdf88c8a2066a5a82afea
mujie <mu-jie@qq.com>
2023-04-15 01:05:24 +0800

    大佬帮助下 光速新增搜索建议功能 •̀ ω •́ ✧

--- html/js/musicPlyr.js
+++ html/js/musicPlyr.js
@@ -341,7 +341,7 @@ function getMusic(rid) {
         lrc = [{ "lineLyric": "纯音乐 请欣赏", "time": "999" }]
       }
     }
-    else if (xhrLrc.status == 433){
+    else if (xhrLrc.status == 433) {
       lrc = [{ "lineLyric": "", "time": "5" }, { "lineLyric": "歌词获取出错，请稍后重试", "time": "999" }]
     }
   }
@@ -463,6 +463,27 @@ if (currentDate.getDay() == 1) {
 } else {
   localStorage.removeItem('Zeroing');
 }
+
+/*搜索建议*/
+if (typeof openSug === "function" && document.getElementById("search_box") != null) openSug("search_box", {
+  //注释的项已在css中定义
+  bgcolor: "#eff6fe",
+  //bgcolorHI : "#e0efda",
+  borderColor: "",
+  fontColor: "",
+  //fontColorHI : "#458bdc",
+  fontFamily: "Montserrat,sans-serif",
+  fontSize: "14px",
+  padding: "6px 25px",
+  radius: "10px",
+  shadow: "rgb(0 0 0 / 50%) 0px 6px 10px",
+  source: "kugou",
+  sugSubmit: true,
+  width: "",
+  XOffset: "",
+  YOffset: "-4"
+}, function () {
+});
 
 
 
